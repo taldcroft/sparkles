69e06c66cd6492a50e26fa39f5fe9949ecca9298
Tom Aldcroft <taldcroft@gmail.com>
2019-03-08 10:55:13 -0500

    Allow correct version within source repo

--- sparkles/__init__.py
+++ sparkles/__init__.py
@@ -1,10 +1,19 @@
-from pkg_resources import get_distribution, DistributionNotFound
+from pkg_resources import get_distribution
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # Package is not installed
-    pass
+    _dist = get_distribution(__name__)
+    __version__ = _dist.version
+
+    if not __file__.startswith(_dist.location):
+        # get_distribution() found a different package from this file, must be in source repo
+        from setuptools_scm import get_version
+        __version__ = get_version(root='..', relative_to=__file__)
+
+except Exception:
+    import warnings
+    warnings.warn('Failed to find a package version, using 0.0.0')
+    __version__ = '0.0.0'
+
 
 from .core import run_aca_review, ACAReviewTable  # noqa
 
