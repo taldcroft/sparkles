1e804293fab2b0b5e200ceb3c6d93a1422cc4cd4
Tom Aldcroft <taldcroft@gmail.com>
2019-03-08 08:57:56 -0500

    Capitalize Package

--- sparkles/__init__.py
+++ sparkles/__init__.py
@@ -3,7 +3,7 @@ from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
-    # package is not installed
+    # Package is not installed
     pass
 
 from .core import run_aca_review, ACAReviewTable  # noqa
